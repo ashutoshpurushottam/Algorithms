268d47afcd9306dbdaa8f1462d6c27552ad40a49
Ashutosh Purushottam <ashutosh.purushottam@mammoth.la>
2019-04-02 17:32:31 +0530

    Some practice Swift code and LinkedList code.

--- 04-LinkedList/LinkedList.playground/Contents.swift
+++ 04-LinkedList/LinkedList.playground/Contents.swift
@@ -1,8 +1,7 @@
 // Copyright (c) 2018 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
-
-example(of: "Creating and linking nodes") {
+example(of: "Of creating node") {
     let node1 = Node(value: 1)
     let node2 = Node(value: 2)
     let node3 = Node(value: 3)
@@ -13,39 +12,20 @@ example(of: "Creating and linking nodes") {
     print(node1)
 }
 
-example(of: "push") {
-    var list = LinkedList<Int>()
-    list.push(3)
-    list.push(2)
-    list.push(1)
+example(of: "Push") {
+    var ll = LinkedList<Int>()
+    ll.push(3)
+    ll.push(2)
+    ll.push(1)
     
-    print(list)
+    print(ll)
 }
 
-example(of: "append") {
+example(of: "Append") {
     var list = LinkedList<Int>()
-    
     list.append(1)
     list.append(2)
     list.append(3)
     
     print(list)
-}
-
-example(of: "Inserting at a particular Index") {
-    var list = LinkedList<Int>()
-    list.push(3)
-    list.push(2)
-    list.push(1)
-    
-    print("Before inserting: \(list)")
-    
-    var midNode = list.node(atIndex: 1)!
-
-    for _ in 1...4 {
-        midNode = list.insert(-1, after: midNode)
-    }
-    
-    print("After inserting: \(list)")
-    
 }
