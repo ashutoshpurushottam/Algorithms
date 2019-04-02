25c7eb0c16804b1a45c8e389821d82140358e7f6
Ashutosh Purushottam <ashutosh.purushottam@mammoth.la>
2019-04-02 17:32:31 +0530

    Some practice Swift code and LinkedList code.

--- 04-LinkedList/LinkedList.playground/Sources/LinkedList.swift
+++ 04-LinkedList/LinkedList.playground/Sources/LinkedList.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct LinkedList<Value> {
+
     public var head: Node<Value>?
     public var tail: Node<Value>?
     
@@ -10,57 +11,30 @@ public struct LinkedList<Value> {
         return head == nil
     }
     
-    public mutating func push(_ value: Value) {
-        head = Node(value: value, next: head)
+    public mutating func push(_ val: Value) {
+        head = Node(value: val, next: head)
         if tail == nil {
             tail = head
         }
     }
     
-    public mutating func append(_ value: Value) {
-        guard !isEmpty else {
-            push(value)
-            return
-        }
-
-        tail!.next = Node(value: value)
-        tail = tail!.next
-    }
-    
-    public func node(atIndex: Int) -> Node<Value>? {
-        var currNode = head
-        var currIndex = 0
-        
-        while currNode != nil && currIndex < atIndex {
-            currNode = currNode!.next
-            currIndex += 1
-        }
-        
-        if(currIndex == atIndex) {
-            return currNode
+    public mutating func append(_ val: Value) {
+        if isEmpty {
+            push(val)
         } else {
-            return nil
+            tail!.next = Node(value: val)
+            tail = tail!.next
         }
-    }
-    
-    @discardableResult
-    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
-        guard node !== tail else {
-            append(value)
-            return tail!
-        }
-        
-        node.next = Node(value: value, next: node.next)
-        return node.next!
     }
 }
 
 extension LinkedList: CustomStringConvertible {
-    
     public var description: String {
         guard let head = head else {
-            return "Empty LinkedList"
+            return "Empty List"
         }
+        
         return String(describing: head)
     }
+
 }
